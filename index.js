bbb1943b25aaa6f4c40039c65c67258262302eac
ElKna <yyt012@gmail.com>
2023-08-12 14:37:44 +0900

    Add: Add dotenv, mysql module and db server connect test

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+'use strict'
+
+require('dotenv').config();
+
+const express   = require('express');
+const mysql     = require('mysql');
+const connection = mysql.createConnection({
+    host        : process.env.HOST_NAME,
+    user        : process.env.USER_NAME,
+    password    : process.env.PASSWORD,
+    database    : process.env.DATABASE,
+})
+const app       = express();
+const port      = 3000;
+
+app.set('port', port)
 
 app.get('/', (req, res) => {
     res.json({
         success: true,
+    });
+});
+
+app.get('/users', (req, res) => {
+    connection.query('SELECT * from Users', (error, rows) => {
+        if (error) throw error;
+        console.log('User info is: ', rows);
+        res.send(rows);
     });
 });
 
