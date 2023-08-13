2f026900858f1e186cb5a501913c4618b9901fd3
ElKna <yyt012@gmail.com>
2023-08-14 01:56:41 +0900

    Add: Add create method

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ const connection = mysql.createConnection(require('./database'))
 const app       = express();
 const port      = 3000;
 app.set('port', port)
+// Express JSON Parse
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`server is listening at localhost:${port}`);
+});
+
+// Sub 3. CreateNew EndPoint
+app.post('/', (req, res) => {
+    if (req.body.hasOwnProperty('TITLE') && req.body.hasOwnProperty('CONTENTS')) {
+        connection.query(`INSERT INTO ${process.env.TABLE_NAME} (TITLE, CONTENTS) VALUES ('${req.body.TITLE}', '${req.body.CONTENTS}')`);
+        res.send(req.body)
+    }
+    else {
+        res.send('Request must be contained TITLE and CONTENTS field')
+    }
 });
 
 // Sub 4. GetAll EndPoint
@@ -25,8 +38,12 @@ app.get('/', (req, res) => {
 app.get('/:id', (req, res) => {
     connection.query(`SELECT * FROM ${process.env.TABLE_NAME} WHERE BOARD_NO='${req.params.id}'`, (error, rows) => {
         if (error) throw error;
-        if (rows.length == 0) {res.send('No exist content')}
-        else {res.send(rows)};
+        if (rows.length == 0) {
+            res.send('No exist content')
+        }
+        else {
+            res.send(rows)
+        };
     })
 })
 
